4def7b3c60430744d309b2776ad0a8f86901181d
kseokc <alvinseok@naver.com>
2025-02-18 09:44:21 +0900

    first initgit add .

---
+++ src/main/java/payroad/PayroadApplication.java
@@ -0,0 +1,13 @@
+package payroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayroadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayroadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/payroad/PayroadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayroadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
