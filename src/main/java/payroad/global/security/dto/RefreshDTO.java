0460b3a2c05d65484e96ee6f344f339ef2d1fcfe
kseokc <alvinseok@naver.com>
2025-03-07 10:57:09 +0900

    feat : 예산관리 입력시 예외코드 구현

---
+++ src/main/java/payroad/global/security/dto/RefreshDTO.java
@@ -0,0 +1,11 @@
+package payroad.global.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RefreshDTO {
+
+    String refreshToken;
+}
