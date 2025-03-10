eec64a04ec2976e82a3dd9d6d8ae7b9ff4c724eb
kseokc <alvinseok@naver.com>
2025-03-10 15:28:06 +0900

    fix : 차트 데이터 조회 swagger 추가

--- src/main/java/payroad/domain/analytics/controller/AnalyticsController.java
+++ src/main/java/payroad/domain/analytics/controller/AnalyticsController.java
@@ -1,5 +1,8 @@
 package payroad.domain.analytics.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import payroad.domain.analytics.dto.AnalyticsResponse;
 import payroad.domain.analytics.dto.AnalyticsResponse.AnalyticsInfoListDTO;
 import payroad.domain.analytics.service.AnalyticsService;
+import payroad.domain.budget.dto.BudgetResponse;
 import payroad.domain.member.Member;
 import payroad.global.response.ApiResponse;
 import payroad.global.security.annotation.LoginMember;
@@ -19,6 +23,14 @@ public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
 
+    @Operation(summary = "통계 차트 데이터 조회 Api", description = "통계차트 데이터 조회 api입니다.<br>**반환 형식(리스트)**<br>")
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(
+        responseCode = "200", description = "성공",
+        content = @Content(schema = @Schema(
+            implementation = AnalyticsResponse.AnalyticsInfoListDTO.class,
+            type = "array",  // List 또는 array로 응답을 나타냄
+            description = "통계 정보 리스트"))
+    )
     @GetMapping
     public ApiResponse<List<AnalyticsResponse.AnalyticsInfoListDTO>> analyticsInfo(
         @LoginMember Member member
