e01d5cf0c1130ed5c89429258b56ffa44b5cb9b5
kseokc <alvinseok@naver.com>
2025-03-11 11:38:14 +0900

    fix : 차트 데이터 수정

--- src/main/java/payroad/domain/analytics/dto/AnalyticsConverter.java
+++ src/main/java/payroad/domain/analytics/dto/AnalyticsConverter.java
@@ -1,13 +1,15 @@
 package payroad.domain.analytics.dto;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import payroad.domain.analytics.dto.AnalyticsResponse.AnalyticsInfoDTO;
 import payroad.domain.analytics.dto.AnalyticsResponse.AnalyticsInfoListDTO;
 import payroad.domain.consumption.Consumption;
-
+@Slf4j
 public abstract class AnalyticsConverter {
 
     public static List<AnalyticsInfoListDTO> toAnalyticsInfoList(
@@ -16,9 +18,11 @@ public abstract class AnalyticsConverter {
     ) {
 
         return totalPrice.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey(Comparator.reverseOrder()))
             .map(stringIntegerMap -> {
                 // 내부 맵을 List<AnalyticsInfoDTO>로 변환
-                List<AnalyticsInfoDTO> analyticsInfoDTOList = stringIntegerMap.getValue().entrySet().stream()
+                List<AnalyticsInfoDTO> analyticsInfoDTOList = stringIntegerMap.getValue().entrySet()
+                    .stream()
                     .map(categoryEntry -> AnalyticsInfoDTO.builder()
                         .category(categoryEntry.getKey())  // 카테고리 이름 설정
                         .percentage((categoryEntry.getValue() * 100.0) / maxTotal)  // 비율 설정
@@ -32,7 +36,5 @@ public abstract class AnalyticsConverter {
                     .analyicsInfoDTOS(analyticsInfoDTOList)
                     .build();
             }).toList();
-
     }
-
 }
