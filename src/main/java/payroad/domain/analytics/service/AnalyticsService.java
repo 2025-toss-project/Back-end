bbc85cb8a945207d13389a9d330e7dc67d937422
kseokc <alvinseok@naver.com>
2025-03-12 09:11:21 +0900

    fix : 트랜잭션 관리 수정

--- src/main/java/payroad/domain/analytics/service/AnalyticsService.java
+++ src/main/java/payroad/domain/analytics/service/AnalyticsService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import payroad.domain.analytics.dto.AnalyticsConverter;
 import payroad.domain.analytics.dto.AnalyticsResponse;
 import payroad.domain.consumption.Consumption;
@@ -17,6 +18,7 @@ import payroad.global.util.CommonUtils;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AnalyticsService {
 
     private final ConsumptionRepository consumptionRepository;
